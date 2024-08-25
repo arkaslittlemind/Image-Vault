b23a0e97165320c68858a6411093238bb4daf231
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2024-08-26 03:01:27 +0530

    refactor: Update top navigation component styling

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -6,7 +6,7 @@ export function TopNav() {
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
 
-      <div className="flex flex-row items-center gap-4 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton />
         </SignedOut>
