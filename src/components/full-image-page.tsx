a3132418c8bb1b586df46c1653111ff4cea30579
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2024-08-27 01:38:36 +0530

    chore: Update Next/Image component usage and npm dependencies

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -5,6 +5,16 @@ import { getImage } from "~/server/queries";
 export default async function FullPageImageView(props: {id: number}){
 
   const image = await getImage(props.id);
-  return <img src={image.url} alt="gallery-image" className="w-96" /> 
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex-shrink flex justify-center items-center">
+        <img src={image.url} alt="gallery-image" className="w-96 object-contain" /> 
+      </div>
+
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  )
   
 }
